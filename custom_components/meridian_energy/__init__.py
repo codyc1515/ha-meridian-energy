23ae363ee0dbb1ff81e71da43480be8a62e522bd
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2023-11-23 09:11:17 +1300

    Allow dynamic unloading

--- custom_components/meridian_energy/__init__.py
+++ custom_components/meridian_energy/__init__.py
@@ -5,24 +5,20 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
 from .api import MeridianEnergyApi
-
-from .const import (
-    DOMAIN,
-    SENSOR_NAME
-)
+from .const import DOMAIN, SENSOR_NAME, PLATFORMS
 
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry
-):
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up from a config entry."""
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
--- custom_components/meridian_energy/const.py
+++ custom_components/meridian_energy/const.py
@@ -1,4 +1,9 @@
 """Constants for the Meridian Energy sensors"""
+from homeassistant.const import Platform
 
 DOMAIN = 'meridian_energy'
 SENSOR_NAME = 'Meridian Energy'
+
+PLATFORMS = [
+    Platform.SENSOR,
+]
