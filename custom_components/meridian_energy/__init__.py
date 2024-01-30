c4b74448b710fed0ca4e708615388b429571711f
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2024-01-30 14:03:28 +1300

    Add reload support

--- custom_components/meridian_energy/__init__.py
+++ custom_components/meridian_energy/__init__.py
@@ -1,11 +1,11 @@
-"""Support for Meridian Energy sensors"""
+"""Support for Meridian Energy sensors."""
+
 import logging
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
-from .api import MeridianEnergyApi
-from .const import DOMAIN, SENSOR_NAME, PLATFORMS
+from .const import DOMAIN, PLATFORMS
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -22,3 +22,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload config entry."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
